05c97892dcda8d3717839303e24f47727b3e659f
Sairam Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2016-07-01 07:51:47 -0400

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -3,14 +3,14 @@ package stock;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import Jama.Matrix;
+import Jama.*;
 
 public class MainClass {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Pca pca= new Pca();
-		Matrix X= pca.SVD();
+	/*	Matrix X= pca.SVD();
 		double[][] arr= X.getArray();
 		FileWriter fw= new FileWriter("/home/helloworld/Documents/outputPCA.csv");
 		
@@ -19,18 +19,20 @@ public class MainClass {
 				fw.write(arr[i][j]+",");
 			}
 			fw.write("\n");
-		};
-		
-/*		double[][] ar= correlation.calculateDistanceMatrix();
-		FileWriter fw= new FileWriter("/home/helloworld/Documents/PCA.csv");
-		
-		for(int i=0; i<6161;i++){
-			for(int j =0; j<6161; j++){
-				fw.write(ar[i][j]+",");
+		}*/
+		Correlation correlation=new Correlation();
+		Matrix dm= pca.SVD();
+		System.out.println("done with svd");
+		double[][] ar = dm.getArray();
+		FileWriter fw= new FileWriter("outputPCA.csv");
+		System.out.println("starting to write");
+		for(int i=0; i<dm.getRowDimension();i++){
+			for(int j =0; j<dm.getColumnDimension(); j++){
+				fw.write(ar[i][j]+"\\s+");
 			}
 			fw.write("\n");
 		}
-		System.out.println(ar[0][6160]);*/
+		//System.out.println(ar[0][6160]);
 	}
 		
 }
